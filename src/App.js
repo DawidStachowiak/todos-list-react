904c05f6028e7a53bdb5a360d8c5b7fbd331366c
Dawid Stachowiak <dawid@zetit.pl>
2021-06-21 10:22:16 +0200

    useEffect added

--- src/App.js
+++ src/App.js
@@ -4,69 +4,75 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState } from "react";
-
-
-
+import { useEffect, useState } from "react";
 
 function App() {
   const [hideTasksDone, setHidetasksDone] = useState(false);
   const [tasks, setTasks] = useState(
-    []
-
+    JSON.parse(localStorage.getItem("tasks")) || []
   );
-    const [tasks, setTasks] = useState 
-    JSON.parse(localStorage.getItem((tasks)) || []);
-    
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
-    setHidetasksDone(hideTasksDone => !hideTasksDone);
-  }
+    setHidetasksDone((hideTasksDone) => !hideTasksDone);
+  };
 
   const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
   };
 
   const toglleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }));
-
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      })
+    );
   };
 
   const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [...tasks,
-    {
-      content,
-      done: false,
-      id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-    }]);
-  }
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
+  };
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form addNewTask={addNewTask} />}
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks}
-          hideTasksDone={hideTasksDone}
-          removeTask={removeTask}
-          toglleTaskDone={toglleTaskDone}
-        />}
+        body={
+          <Tasks
+            tasks={tasks}
+            hideTasksDone={hideTasksDone}
+            removeTask={removeTask}
+            toglleTaskDone={toglleTaskDone}
+          />
+        }
         extraHeaderContent={
-          <Buttons tasks={tasks}
+          <Buttons
+            tasks={tasks}
             hideTasksDone={hideTasksDone}
             toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone} />
+            setAllDone={setAllDone}
+          />
         }
       />
     </Container>
@@ -74,4 +80,3 @@ function App() {
 }
 
 export default App;
-
